2a53ddd77e83e05d938e04ae4a5e35cd65c8a1f0
nema <40147733+nexnema@users.noreply.github.com>
2021-09-17 15:08:01 +0430

    Upload

---
+++ style.php
@@ -0,0 +1,20 @@
+<?php
+
+    header("Content-Type: text/css");
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
+
+    require 'template.css';
+    require 'header.css';
+    require 'product.css';
+    require 'products.css';
+    require 'post.css';
+    require 'footer.css';
+    require 'account.css';
+    require 'posts.css';
+    require 'category.css';
+    require 'cart.css';
+    require 'search.css';
+
+
+?>
